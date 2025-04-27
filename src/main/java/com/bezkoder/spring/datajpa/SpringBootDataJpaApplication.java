a5d5ade6f9186f10576064b90cd545a968a6a7a3
duongvanhoan <115551022+DuongHoan35678888@users.noreply.github.com>
2025-04-27 21:55:07 +0700

    update

--- src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
+++ src/main/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplication.java
@@ -2,12 +2,19 @@ package com.bezkoder.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDataJpaApplication {
+public class SpringBootDataJpaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootDataJpaApplication.class);
 	}
 
 }
